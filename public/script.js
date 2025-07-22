a89cac231ca6e095ee8532711879cb96bdab2c39
Pic0777 <162361580+Pic0777@users.noreply.github.com>
2025-07-22 12:05:09 -0300

    corrigindo

--- public/script.js
+++ public/script.js
@@ -1,5 +1,15 @@
 let currentUserId = localStorage.getItem("userId") || null;
 
+// Mostra o perfil
+function mostrarPerfil(name) {
+  const profile = document.getElementById("profile");
+  profile.innerHTML = ""; // Limpa inputs
+  const h1 = document.createElement("h1");
+  h1.innerHTML = `${name}`;
+  profile.appendChild(h1);
+}
+
+// Registrar usuário
 async function registerUser() {
   const name = document.getElementById("nameInput").value;
   if (!name) return alert("Digite um nome!");
@@ -16,19 +26,10 @@ async function registerUser() {
   localStorage.setItem("userName", name);
 
   alert(`Usuário registrado como ${name}`);
-
-  window.onload = function () {
-  const savedName = localStorage.getItem("userName");
-  if (savedName) {
-    const profile = document.getElementById("profile");
-    profile.innerHTML = ""; // limpa a área
-    const h1 = document.createElement("h1");
-    h1.innerHTML = `${savedName}`;
-    profile.appendChild(h1);
-  }
-};
+  mostrarPerfil(name);
 }
 
+// Buscar mensagens
 async function fetchMessages() {
   const res = await fetch("/messages");
   const data = await res.json();
@@ -41,6 +42,7 @@ async function fetchMessages() {
   });
 }
 
+// Enviar mensagens
 async function sendMessage() {
   if (!currentUserId) {
     return alert("Você precisa registrar um nome antes de enviar mensagens.");
@@ -54,16 +56,25 @@ async function sendMessage() {
     body: JSON.stringify({ userId: currentUserId, text })
   });
 
-  document.getElementById("messageInput").value = ""; // <-- corrigido "messsageInput"
+  document.getElementById("messageInput").value = "";
   fetchMessages();
 }
 
+// Evento para enviar com Enter
 const input = document.getElementById("messageInput");
-  input.addEventListener("keydown", function (event){
-    if (event.key === "Enter") {
-    sendMessage(); // Chama a função de enviar mensagem
-    }
-  })
+input.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    sendMessage();
+  }
+});
+
+// Quando a página carregar
+window.onload = function () {
+  const savedName = localStorage.getItem("userName");
+  if (savedName) {
+    mostrarPerfil(savedName);
+  }
+};
 
 setInterval(fetchMessages, 5000);
 fetchMessages();
